e8c058c42f33be278c5646b973f91258f8413eb3
fourat8 <48157826+fourat8@users.noreply.github.com>
2019-03-27 16:53:50 -0700

    Update score.c

--- score.c
+++ score.c
@@ -1,61 +1,47 @@
 #include"score.h"
-void init_score(score *s,char vie[])
-{int i;
+void initialiservie(vie *vie)
+{
+vie->nb=3;
+vie->position.x = -600 ;
+vie->position.y = 0 ;
+vie->fond1 = IMG_Load("vie1/v1.png");
+vie->fond2 = IMG_Load("vie1/v2.png");
+vie->fond3 = IMG_Load("vie1/v3.png");
+vie->fond4 = IMG_Load("vie1/v4.png");
+vie->fond5 = IMG_Load("GameOver.png");
+}
+void affichervie(vie *vie,SDL_Surface *ecran)
+{
+if(vie->nb==3)
+{
+SDL_BlitSurface(vie->fond1,NULL, ecran,&vie->position);
+
+}
+if(vie->nb==2)
+{
+SDL_BlitSurface(vie->fond2,NULL, ecran,&vie->position);
+}
+if(vie->nb==1)
+{
+SDL_BlitSurface(vie->fond3,NULL, ecran,&vie->position);
+}
+if(vie->nb==0)
+{
+SDL_BlitSurface(vie->fond4,NULL, ecran,&vie->position);
+SDL_BlitSurface(vie->fond5,NULL, ecran,&vie->position);
+
+SDL_Flip(ecran);
+
+}
+}
+void free_score(vie *vie)
+{
+      
+
+	SDL_FreeSurface(vie->fond1);
+	SDL_FreeSurface(vie->fond2);	
+	SDL_FreeSurface(vie->fond3);	
+	SDL_FreeSurface(vie->fond4);	
+	SDL_FreeSurface(vie->fond5);		
 	
-	s->pos1.x=400;
-	s->pos1.y=10;
-	s->pos2.x=340;
-	s->pos2.y=10;
-        s->pos3.x=280;
-	s->pos3.y=10;
-	s->pos.x=220;
-	s->pos.y=10;
-        s->nbr_de_vie=3;
-for(i=0;i<3;i++)
-{
-      sprintf(vie,"vie%d.png",i);
-      s->coeur1[i]=IMG_Load(vie);
-}}
-void affichage_score0(score *s,SDL_Surface *ecran,int *i)
-{
-
-if (*i>50)
-*i=0;
-        SDL_BlitSurface(s->coeur1[*i],NULL,ecran,&s->pos1);
-	SDL_BlitSurface(s->coeur1[*i],NULL,ecran,&s->pos2);
-	SDL_BlitSurface(s->coeur1[*i],NULL,ecran,&s->pos3);
-(*i)++;
-
-       SDL_Delay(15);
-	//SDL_Flip(ecran);
-
-
-}
-void affichage_score1(score *s,SDL_Surface *ecran,int *i)
-{
-
-if (*i>50)
-*i=0;  
-	SDL_BlitSurface(s->coeur1[*i],NULL,ecran,&s->pos1);
-	SDL_BlitSurface(s->coeur1[*i],NULL,ecran,&s->pos2);
-	
-(*i)++;
-       SDL_Delay(15);
-     	
-}
-void affichage_score2(score *s,SDL_Surface *ecran,int *i)
-{
-if (*i>50)
-*i=0;	  
-	SDL_BlitSurface(s->coeur1[*i],NULL,ecran,&s->pos3);
-(*i)++;
-       SDL_Delay(15);
-    	
-}
-void free_score(score *s)
-{int i;
-      for(i=0;i<3;i++){
-
-	SDL_FreeSurface(s->coeur1[i]);	
-	}
 }
