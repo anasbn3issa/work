e0c668f2af5bda19d5405f3f5637f1495fd0ba46
Ahmedelkelb27031999 <48155787+Ahmedelkelb27031999@users.noreply.github.com>
2019-03-27 10:28:31 +0100

    Update and rename movement.h to player.h

--- movement.h
+++
@@ -1,9 +0,0 @@
-#ifndef SCROLLING_H_INCLUDED
-#define SCROLLING_H_INCLUDED
-#include "initbackground.h"
-#include "scrolling.h"
-Uint8 *key;
-SDL_Event event;
-void move(SDL_Event event,SDL_Surface *screen,*background,*image2,SDL_Rect position2,backgroundposition);
-void movems(SDL_Event event,SDL_Surface *screen,*background,*image2,SDL_Rect position2,backgroundposition);
-#endif //SCROLLING_H_INCLUDED
