39ef2428ddc3145bd025a46b0513fea2cc8d13a2
Ahmedelkelb27031999 <48155787+Ahmedelkelb27031999@users.noreply.github.com>
2019-03-27 10:26:17 +0100

    Update initbackground.h

--- initbackground.h
+++ initbackground.h
@@ -1,20 +1,18 @@
-#ifndef SCROLLING_H_INCLUDED
-#define SCROLLING_H_INCLUDED
-#include <SDL/SDL.h>
+#ifndef _MAP_H_INCLUDED
+#define _MAP_H_INCLUDED
+#include "player.h"
+#include <stdlib.h>
+#include <stdio.h>
 #include <SDL/SDL_image.h>
-#pragma comment(lib,"SDL.lib")
-#pragma comment(lib,"sdlmain.lib")
-#define SPEED 1
-#define IMX 2900
-#define IMY 2900
-SDL_Surface*background,*screen,*image2;
-SDL_Rect position2;
-SDL_Rect backgroundposition;
-int x,y;
-position2.x = 500;
-position2.y = 500;
-backgroundposition.x = 0;
-backgroundposition.y = 0;
-x=y=0;
-SDL_Surface *loading(const char *fic);
-#endif //SCROLLING_H_INCLUDED
+#include <SDL/SDL.h>
+#include <SDL/SDL_ttf.h>
+#include <SDL/SDL_mixer.h>
+
+typedef struct map
+{
+  SDL_Surface *image;
+  SDL_Rect position;  
+}map;
+void initialisermap(map *map);
+void affichermap(map *map,SDL_Rect camera,SDL_Surface *ecran);
+#endif // MAP_H_INCLUDED
