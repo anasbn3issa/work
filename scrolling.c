e4bd905b0b31541f9c04797b723a23bcd090fbd9
Ahmedelkelb27031999 <48155787+Ahmedelkelb27031999@users.noreply.github.com>
2019-03-27 10:30:19 +0100

    Update scrolling.c

--- scrolling.c
+++ scrolling.c
@@ -1,43 +1,12 @@
+#include<SDL/SDL.h>
+#include<SDL/SDL_image.h>
+#include<SDL/SDL_mixer.h>
 #include "scrolling.h"
-void scrolling(int x,int y,int IMX,int IMY,int SPEED,camera *c)
+
+void scrolling (SDL_Rect *camera,joueur j)
 {
-do
-{
-key=SDL_GetKeyState(NULL);
-SDL_PumpEvents();
-if(key[SDLK_LEFT])
-{
- x+=SPEED;
-}
-if(key[SDLK_RIGHT])
-{
- x-=SPEED;
-}
-if(key[SDLK_UP])
-{
- y+=SPEED;
-}
-if(key[SDLK_DOWN])
-{
- y-=SPEED;
-}
-c->positioncamera.x=negatmodulo(x,IMX);
-c->positioncamera.y=negatmodulo(y,IMY);
-SDL_BlitSurface(background,NULL,screen,&positioncamera);
-c->positioncamera.x=negatmodulo(x,IMX)-IMX;
-c->positioncamera.y=negatmodulo(y,IMY);
-SDL_BlitSurface(background,NULL,screen,&positioncamera);
-c->positioncamera.x=negatmodulo(x,IMX)-IMX;
-c->positioncamera.y=negatmodulo(y,IMY)-IMY;
-SDL_BlitSurface(background,NULL,screen,&positioncamera);
-c->positioncamera.x=negatmodulo(x,IMX);
-c->positioncamera.y=negatmodulo(y,IMY)-IMY;
-SDL_Flip(screen);
-SDL_Delay(3);
-}while(!key[SDLK_ESCAPE]);
-int negatmodulo(int x,int mod)
-{
-if(x>=0)
-  return x%mod;
-return (x%mod)+mod;
+  camera->x = 2*2*2*(j.position).x;
+  camera->y = 2*2*2*(j.position).y;
+camera->h=800;
+camera->w=800;
 }
