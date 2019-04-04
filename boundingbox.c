05a4646ffd2aeee044207d288e43f0b45c3d2cbf
fourat8 <48157826+fourat8@users.noreply.github.com>
2019-04-04 03:13:30 -0700

    Update boundingbox.c

--- boundingbox.c
+++ boundingbox.c
@@ -1,6 +1,13 @@
-int collision(AABB box1,AABB box2)
+#include <stdlib.h>
+#include <stdio.h>
+#include <SDL/SDL_image.h>
+#include <SDL/SDL.h>
+#include <SDL/SDL_ttf.h>
+#include <SDL/SDL_mixer.h>
+#include "boundingbox.h"
+int collision(SDL_Rect box1,SDL_Rect box2)
 {
-if ((box2.x>=box1.X + box1.w) // trop a droite
+if ((box2.x>=box1.x + box1.w) // trop a droite
 || (box2.x += box2.w <= box1.x)//trop a gauche
 || (box2.y >= box1.y + box1.h)// trop en bas
 || (box2.y + box2.h <= box1.h))//trop en haut
