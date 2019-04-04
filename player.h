b4510f02d9d4ddf71fed44cf39abdb753b571a9b
eskander1998 <47850204+eskander1998@users.noreply.github.com>
2019-04-04 11:11:55 +0100

    Update and rename personnage.h to player.h

---
+++ player.h
@@ -0,0 +1,27 @@
+#ifndef PLAYER_H_INCLUDED
+#define PLAYER_H_INCLUDED
+#include <stdlib.h>
+#include <stdio.h>
+#include <SDL/SDL_image.h>
+#include <SDL/SDL.h>
+#include <SDL/SDL_ttf.h>
+#include <SDL/SDL_mixer.h>
+#include "map.h"
+
+typedef struct //joueur
+{
+
+  SDL_Surface *im;
+  SDL_Rect position;
+
+}joueur; 
+
+
+void initjoueur (joueur *j);/*initialisation de joueur*/
+void afficherjoueur(joueur *j,SDL_Surface *ecran);
+void deplacementjoueur (joueur *j,SDL_Event event,int i,SDL_Surface *surface);/*deplacement de personage*/
+int collisionjoueur (SDL_Surface* image,joueur *j,int d);
+SDL_Color GetPixel ( SDL_Surface* pSurface , int x , int y);
+
+#endif // AFFICHECHAR_H_INCLUDED
+
