c753c4bfdc6dd05e7b4a2db8b12729bdee97dc3e
anasbn3issa <47992691+anasbn3issa@users.noreply.github.com>
2019-04-04 11:13:19 +0100

    Update enigmes.c

--- enigmes.c
+++ enigmes.c
@@ -1,21 +1,13 @@
-#include <stdio.h>
-#include <SDL/SDL.h>
-#include <SDL/SDL_image.h>
 #include "enigmes.h"
-#include <stdlib.h>
-#include <time.h>
-#include "personage.h"
 
 
 void init_enigmes(posenigme *pose,enigme *eni )
 {
-    pose->posS.x=50;
-    pose->posS.y=50;
+    pose->posS.x=0;
+    pose->posS.y=0;
     pose->posQ.x = 0;
     pose->posQ.y = 0;
-SDL_Rect aux;
-aux.x=0;
-aux.y=0;
+
     eni->E1= IMG_Load("imageenigme/enigme1.png");
     eni->E2= IMG_Load("imageenigme/enigme2.png");
     eni->E3= IMG_Load("imageenigme/enigme3.png");
@@ -30,8 +22,6 @@ aux.y=0;
 
 void generation_auto(posenigme *pose,enigme *eni, SDL_Surface *ecran,int chamb)
 {
-    /*srand(time(NULL));
-    chamb=rand()%4;*/
     switch (chamb)
     {
     case 0 :
@@ -127,37 +117,40 @@ void init_resolution(enigme *eni)
 }
 
 
-void resolution_enigmes(posenigme *pose,enigme *eni, SDL_Surface *ecran,SDL_Event event, joueur *j,int *mistake)
+void resolution_enigmes(posenigme *pose,enigme *eni, SDL_Surface *ecran, joueur *j,int *mistake)
 {
+SDL_Event event;
 int continuer=1;
 while (continuer)
     { 
        SDL_PollEvent(&event);
 	switch(event.type)
             {
+		case SDL_QUIT: 
+		continuer = 0;
+		break;
 
 		case SDL_MOUSEBUTTONDOWN:
                 if (event.button.button == SDL_BUTTON_LEFT)
                 {
-                    if(event.button.x>53 && event.button.x<=371 && event.button.y>=692 && event.button.y <=741)
+                    if(event.button.x>30 && event.button.x<=207 && event.button.y>=532 && event.button.y <=570)
                     {
                         SDL_BlitSurface(eni->key, NULL, ecran, &pose->posS);
                         SDL_Flip(ecran);SDL_Delay(500);
 
                       continuer=0;
-		      j->inv=1;
 			break;
 
                     }
 
-                    else if((*mistake)==0&&event.button.x>1055 && event.button.x<=1173&& event.button.y>=693 && event.button.y <=742)
+                    else if((*mistake)==0&&event.button.x>591 && event.button.x<=767&& event.button.y>=530 && event.button.y <=566)
                     {
                         SDL_BlitSurface(eni->try1, NULL, ecran, &pose->posS);
                         SDL_Flip(ecran);SDL_Delay(500);continuer=0;
 			(*mistake)++;
 			break;
                     }
-		    else if((*mistake)==1&&event.button.x>1055 && event.button.x<=1173&& event.button.y>=693 && event.button.y <=742)
+		    else if((*mistake)==1&&event.button.x>591 && event.button.x<=767&& event.button.y>=530 && event.button.y <=566)
                     {
                         SDL_BlitSurface(eni->lose, NULL, ecran, &pose->posS);
                         SDL_Flip(ecran);SDL_Delay(500);continuer=0;
@@ -176,4 +169,3 @@ while (continuer)
 
 
 } //end func
-
