e8093c86586694843883c54e3e9abd9979b4da27
anasbn3issa <47992691+anasbn3issa@users.noreply.github.com>
2019-04-04 11:13:39 +0100

    Update enigmes.h

--- enigmes.h
+++ enigmes.h
@@ -1,6 +1,11 @@
 #ifndef ENIGMES_H_INCLUDED
 #define ENIGMES_H_INCLUDED
-#include"personage.h"
+#include <stdio.h>
+#include <SDL/SDL.h>
+#include <SDL/SDL_image.h>
+#include <stdlib.h>
+#include <time.h>
+#include "player.h"
 typedef struct 
 {      
 	SDL_Rect posQ,posS;
@@ -18,7 +23,7 @@ typedef struct
 void init_enigmes(posenigme *pose,enigme *eni );
 void generation_auto(posenigme *pose,enigme *eni, SDL_Surface *ecran,int chamb);
 void init_resolution(enigme *eni);
-void resolution_enigmes(posenigme *pose,enigme *eni, SDL_Surface *ecran,SDL_Event event, joueur *j,int *mistake);
+void resolution_enigmes(posenigme *pose,enigme *eni, SDL_Surface *ecran, joueur *j,int *mistake);
 
 
 #endif // ENIGMES_H_INCLUDED
