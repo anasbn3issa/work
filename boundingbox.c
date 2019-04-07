9bff27bf97e42c5497f47431630278836447bfdc
fourat8 <48157826+fourat8@users.noreply.github.com>
2019-04-07 07:46:33 -0700

    Update boundingbox.c

--- boundingbox.c
+++ boundingbox.c
@@ -9,11 +9,11 @@ int collision(SDL_Rect box1,SDL_Rect box2)
 {
 if(box1.y>=box2.y+box2.h)
 return 0;
-if (box1.x>=box2.x+box2.w-30)
+if (box1.x>=box2.x+box2.w)
 return 0;
 if (box1.y+box1.h<=box2.y)
 return 0;
-if (box1.x+box1.w-30<=box2.x)
+if (box1.x+box1.w<=box2.x)
 return 0;
 return 1;
 }
