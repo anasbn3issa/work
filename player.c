23c4c5df34ba9c16758f77526dd6206925e286e0
eskander1998 <47850204+eskander1998@users.noreply.github.com>
2019-04-07 15:48:37 +0100

    Update player.c

--- player.c
+++ player.c
@@ -1,7 +1,7 @@
 
 #include "player.h"
 
-//#include "collision.h"
+
 
 SDL_Color GetPixel ( SDL_Surface* pSurface , int x , int y)
 {
@@ -16,73 +16,75 @@ SDL_Color GetPixel ( SDL_Surface* pSurface , int x , int y)
 }
 
 
-int collisionjoueur (SDL_Surface* image,joueur *j,int d)
+
+int collisionjoueur(SDL_Surface *image,joueur *j, int d)
+{ SDL_Color color[3];
+int i;
+switch(d)
 {
+case 1: //droite
+  color [0] = GetPixel (image, j->position.x +j->position.w, j->position.y);
+  color [1] = GetPixel (image, j->position.x +j->position.w, j->position.y +j->position.h /2);
+  color [2] = GetPixel (image, j->position.x +j->position.w, j->position.y +j->position.h);
+break;
+case 2://bas
+  color [0] = GetPixel (image,j->position.x, j->position.y +j->position.h);
+  color [1] = GetPixel (image, j->position.x +j->position.w /2, j->position.y+j->position.h);
+  color [2] = GetPixel (image, j->position.x +j->position.w, j->position.y +j->position.h);
+break;
+case 3://haut
+  color [0] = GetPixel (image, j->position.x, j->position.y);
+  color [1] = GetPixel (image, (j->position.x+j->position.w) /2, j->position.y);
+  color [2] = GetPixel (image, j->position.x +j->position.w, j->position.y);
+break;
+case 4: //gauche
+  color [0] = GetPixel (image, j->position.x, j->position.y);
+  color [3] = GetPixel (image, j->position.x, j->position.y +j->position.h /2);
+  color [4] = GetPixel (image, j->position.x, j->position.y +j->position.h);
+break;
 
-SDL_Color p;
-
-float X,Y;
-switch (d)
-  {
-   case 1 : //down
-	//le point n 6 du perso ne doit pas toucher le rouge
-     X= (j->position.x+j->position.w)/2;
-     Y= j->position.x+j->position.h;
-     break;
-   case 2 ://up
-   //le point n 6 du perso ne doit pas toucher le rouge	
-     X= (j->position.x+j->position.w)/2;
-     Y= j->position.x+j->position.h;
-     break;
-   case 3 ://right
-	//le point n 8 du perso ne doit pas toucher le rouge	
-     X= j->position.x +j->position.w;
-     Y= j->position.x+((j->position.h)/2);
-     break;       
-   case 4 :// left
-	//le point n 4 du perso ne doit pas toucher le rouge	
-     X= j->position.x ;
-     Y= j->position.y+((j->position.h)/2) ;
-     break;
-  }
-
-
-p=GetPixel (image,X,Y);
-if (((p.r==255)&&(p.b==0)&&(p.g==0)))
-{printf("0");
-	return 1;}
-else 
-	return 0;
 }
+
+for(i=0;i<3;i++)
+{
+if ((color[i].r==255 && color[i].g==0 && color[i].b==0)) return 1; else return 0;
+}
+}
+
 
 void initjoueur (joueur *j)
 
 {
     
     j->position.x =40;//40;
-    j->position.y =55;//55 ;
-    j->position.h =138;
-    j->position.w =478;
+    j->position.y =60;//55 ;
+    j->position.h =115;
+    j->position.w =400;
      j->im=IMG_Load("EG.png");
 }
 
-
+void afficherjoueur(joueur *j,SDL_Surface *ecran)
+{
+ SDL_BlitSurface(j->im, NULL, ecran, &j->position);
+}
 
 
 void deplacementjoueur (joueur *j,SDL_Event event,int i,SDL_Surface *surface)
 {
-int d=0,c=0;
+int d=0;
  if (event.type == SDL_KEYDOWN)
  {
    if (event.key.keysym.sym == SDLK_DOWN)
+		 {
+
+				
+                         d=2;
+				if(!collisionjoueur(surface,j,d))
 			{
- 				d=1;
-                           c=collisionjoueur (surface,j,d);		//collisionmap (d,j->position,surface);
-                           if (c==0)
-                              {
-                         
 				j->position.y +=1;
-			 switch(i)
+
+ 				switch(i)
+                          
                           {case 1:
                            j->im=IMG_Load("EG.png");
                            break;
@@ -90,18 +92,16 @@ int d=0,c=0;
                            j->im=IMG_Load("ED.png");
                             break;
                            } 
-                   	} //end if
-else printf("1");
-                          
+
+			} else ("stop down\n");
                           
                          
-                        }
+                 }
   else if(event.key.keysym.sym == SDLK_UP)
                         {
-				d=2;
-                            c=collisionjoueur (surface,j,d);
-                           if (c==0)
-                              {
+    d=3;
+				if(!collisionjoueur(surface,j,d))
+				{
                          
 				j->position.y -=1;
 
@@ -114,17 +114,17 @@ else printf("1");
                            j->im=IMG_Load("ED.png");
                             break;
                            } }
-else printf("rtyuffrtyh.");
+
                    
                           
               
                         }
   else if(event.key.keysym.sym == SDLK_RIGHT)
                         {
-				d=3;
-                          c=collisionjoueur (surface,j,d);
-                           if (c==0)
-                              {
+
+    d=1;
+				if(!collisionjoueur(surface,j,d))				
+			{
                           j->position.x +=1;
 				
                           switch(i)
@@ -135,16 +135,15 @@ else printf("rtyuffrtyh.");
                            case 2:
                            j->im=IMG_Load("ED.png");
                             break;
-                           }}else printf("3");
+                           }}
                    
                           
                         }
   else if(event.key.keysym.sym == SDLK_LEFT)
                         {
-					d=4;
-                          c=collisionjoueur (surface,j,d);
-                           if (c==0)
-                              {
+    d=4;
+				if(!collisionjoueur(surface,j,d))
+				{
                           j->position.x -=1;
 				
                           switch(i)
@@ -154,20 +153,10 @@ else printf("rtyuffrtyh.");
                            case 2:
                            j->im=IMG_Load("REG.png");
                             break;
-                           }}else printf("4");
+                           }}
                         }
 
  }
 
 }
-
-
-
-
-void afficherjoueur(joueur *j,SDL_Surface *ecran)
-{
- SDL_BlitSurface(j->im, NULL, ecran, &j->position);
-}
-
-
 
